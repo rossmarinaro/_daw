7aa396cd2dfe27050279af7f55b9a4c2b498e2a5
Ross <44373734+rossmarinaro@users.noreply.github.com>
2021-09-14 20:57:25 -0400

    Update main.js

--- src/daw/main/main.js
+++ src/daw/main/main.js
@@ -1,12 +1,15 @@
 import { SAMPLER } from '../instruments/sampler/ui.js';
 import { KEYBOARD } from '../instruments/keyboard/ui.js';
-
+import { AMP } from '../main/amp.js';
 
 
 export const INSTRUMENT_RACK = {
   Instruments : {
     sampler: SAMPLER,
     keyboard: KEYBOARD
+  },
+  Amps: {
+    default: AMP
   },
   Defaults : {
     instrument: SAMPLER,
@@ -17,6 +20,13 @@ export const INSTRUMENT_RACK = {
             div.setAttribute('id', "instrument-rack");
             document.body.appendChild(div);
             INSTRUMENT_RACK.Defaults.instrument.Init();
+            document.addEventListener('keydown', e =>{
+              if (e.code === 'KeyA') 
+              {
+                window.location.replace('https://rossmarinaro.github.io/daw/amp.html');
+                console.log('amp initialized');
+              }
+            });
     },
     
   }
