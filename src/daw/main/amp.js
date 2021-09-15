10e5b91ee6f25eac0a52398948e3fdeaee29fdfd
Ross <44373734+rossmarinaro@users.noreply.github.com>
2021-09-14 20:31:01 -0400

    Add files via upload

--- src/daw/main/amp.js
+++ src/daw/main/amp.js
@@ -1,111 +1,100 @@
+/******* AMP *******/ 
 
-const volume = document.getElementById('volume')
-const bass = document.getElementById('bass')
-const mid = document.getElementById('mid')
-const treble = document.getElementById('treble')
-const visualizer = document.getElementById('visualizer')
+const AMP = {
+  features: {
+    volume: document.getElementById('volume'),
+    bass: document.getElementById('bass'),
+    mid: document.getElementById('mid'),
+    treble: document.getElementById('treble'),
+    visualizer: document.getElementById('visualizer'),
+    context: new AudioContext(),
+    analyserNode: new AnalyserNode(context, {fftSize: 256}),
+    gainNode: new GainNode(context, { gain: volume.value}),
+    bassEQ: new BiquadFilterNode(context, {type: 'lowshelf', frequency: 500, gain: bass.value}),
+    midEQ: new BiquadFilterNode(context, {type: 'peaking', Q: Math.SQRT1_2, frequency: 1500, gain: mid.value}),
+    trebleEQ: new BiquadFilterNode(context, {type: 'highshelf', frequency: 3000, gain: treble.value})
+  },
+  init: function()
+  {
+    AMP.setupEventListeners();
+    AMP.setupContext();
+    AMP.resize();
+    AMP.drawVisualizer();
+  },
+  setupContext: async function() 
+  {
+    const guitar = await getGuitar();
+    if (context.state === 'suspended') await context.resume();
+    /* const source =  */context.createMediaStreamSource(guitar)
+    //source
+      .connect(AMP.features.bassEQ)
+      .connect(AMP.features.midEQ)
+      .connect(AMP.features.trebleEQ)
+      .connect(AMP.features.gainNode)
+      .connect(AMP.features.analyserNode)
+      .connect(AMP.features.context.destination);
+  },
+  getGuitar: function() 
+  {
+    return navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: false,
+        autoGainControl: false,
+        noiseSuppression: false,
+        latency: 0
+      }
+    })
+  },
+  drawVisualizer: function() 
+  {
+    AMP.requestAnimationFrame(drawVisualizer);
+    const bufferLength = analyserNode.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+    analyserNode.getByteFrequencyData(dataArray)
+    const width = AMP.features.visualizer.width
+    const height = AMP.features.visualizer.height
+    const barWidth = width / bufferLength
 
-const context = new AudioContext()
-const analyserNode = new AnalyserNode(context, { fftSize: 256 })
-const gainNode = new GainNode(context, { gain: volume.value})
-const bassEQ = new BiquadFilterNode(context, {
-  type: 'lowshelf',
-  frequency: 500,
-  gain: bass.value
-})
-const midEQ = new BiquadFilterNode(context, {
-  type: 'peaking',
-  Q: Math.SQRT1_2,
-  frequency: 1500,
-  gain: mid.value
-})
-const trebleEQ = new BiquadFilterNode(context, {
-  type: 'highshelf',
-  frequency: 3000,
-  gain: treble.value
-})
+    const canvasContext = visualizer.getContext('2d')
+    canvasContext.clearRect(0, 0, width, height)
 
-setupEventListeners()
-setupContext()
-resize()
-drawVisualizer()
+    dataArray.forEach((item, index) => {
+      const y = item / 255 * height / 2
+      const x = barWidth * index
 
-function setupEventListeners() {
-  window.addEventListener('resize', resize)
+      canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
+      canvasContext.fillRect(x, height - y, barWidth, y)
+    })
+  },
+  setupEventListeners: function() 
+  {
+    window.addEventListener('resize', resize)
 
-  volume.addEventListener('input', e => {
-    const value = parseFloat(e.target.value)
-    gainNode.gain.setTargetAtTime(value, context.currentTime, .01)
-  })
-
-  bass.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    bassEQ.gain.setTargetAtTime(value, context.currentTime, .01)
-  })
-
-  mid.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    midEQ.gain.setTargetAtTime(value, context.currentTime, .01)
-  })
-
-  treble.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    trebleEQ.gain.setTargetAtTime(value, context.currentTime, .01)
-  })
-}
-
-async function setupContext() {
-  const guitar = await getGuitar()
-  if (context.state === 'suspended') {
-    await context.resume()
+    AMP.features.volume.addEventListener('input', e => {
+      const value = parseFloat(e.target.value)
+      gainNode.gain.setTargetAtTime(value, context.currentTime, .01)
+    });
+    AMP.features.bass.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      bassEQ.gain.setTargetAtTime(value, context.currentTime, .01)
+    });
+    AMP.features.mid.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      midEQ.gain.setTargetAtTime(value, context.currentTime, .01)
+    });
+    AMP.features.treble.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      trebleEQ.gain.setTargetAtTime(value, context.currentTime, .01)
+    });
+  },
+  resize: function() 
+  {
+    AMP.features.visualizer.width = AMP.features.visualizer.clientWidth * window.devicePixelRatio
+    AMP.features.visualizer.height = AMP.features.visualizer.clientHeight * window.devicePixelRatio
   }
-  const source = context.createMediaStreamSource(guitar)
-  source
-    .connect(bassEQ)
-    .connect(midEQ)
-    .connect(trebleEQ)
-    .connect(gainNode)
-    .connect(analyserNode)
-    .connect(context.destination)
 }
 
-function getGuitar() {
-  return navigator.mediaDevices.getUserMedia({
-    audio: {
-      echoCancellation: false,
-      autoGainControl: false,
-      noiseSuppression: false,
-      latency: 0
-    }
-  })
-}
 
-function drawVisualizer() {
-  requestAnimationFrame(drawVisualizer)
 
-  const bufferLength = analyserNode.frequencyBinCount
-  const dataArray = new Uint8Array(bufferLength)
-  analyserNode.getByteFrequencyData(dataArray)
-  const width = visualizer.width
-  const height = visualizer.height
-  const barWidth = width / bufferLength
 
-  const canvasContext = visualizer.getContext('2d')
-  canvasContext.clearRect(0, 0, width, height)
-
-  dataArray.forEach((item, index) => {
-    const y = item / 255 * height / 2
-    const x = barWidth * index
-
-    canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
-    canvasContext.fillRect(x, height - y, barWidth, y)
-  })
-}
-
-function resize() {
-  visualizer.width = visualizer.clientWidth * window.devicePixelRatio
-  visualizer.height = visualizer.clientHeight * window.devicePixelRatio
-}
-
-  
 
